7d98edb06c484e5e043ffece4a145e38b6b78972
CallsignOpossum <88398441+CallsignOpossum@users.noreply.github.com>
2021-08-11 20:31:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+#
+# This file contains the main function.
+
+def main():
+    pass
+
+# Check to see if this file was imported or ran directly.  If ran directly call
+# main.
+if __name__ == '__main__':
+    main()
